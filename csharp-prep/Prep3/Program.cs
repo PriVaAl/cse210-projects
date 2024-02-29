5ab71a7ae6be903be8c14e7b8cf47cfc55e22244
Priscilla Valverde <priscilla.valverde@gmail.com>
2024-02-28 19:29:48 -0600

    Guessing number

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Console.Write("What is the magic number?: ");
+        string magic = Console.ReadLine();
+        int  number = int.Parse(magic);
+
+        Console.Write("What is your guess?:");
+        string response = Console.ReadLine();
+        int guess = int.Parse(response);
+        int max = 100;
+
+        while (guess>= max)
+        {
+            if (guess == number)
+            {
+                Console.WriteLine("Congratulations! You have guessed the number.");
+            }
+            else if (guess > number )
+            {
+                Console.WriteLine("Higher.");
+            }
+            else
+            {
+                Console.WriteLine("Lower");
+            }
+        }
+
     }
 }

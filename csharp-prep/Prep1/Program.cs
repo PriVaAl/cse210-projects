b0b4ab1f9600a2270111400497d69c18118f675c
Priscilla Valverde <priscilla.valverde@gmail.com>
2024-02-28 18:06:44 -0600

    Add name

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.Write("What's your first name: ");
+        string name = Console.ReadLine();
+
+        Console.Write("What's your last name: ");
+        string last_name = Console.ReadLine();
+        
+        Console.WriteLine($"Your name is {last_name}, {last_name} {name}");
     }
 }

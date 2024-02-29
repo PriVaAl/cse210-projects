107bfbbea4d1e76306673c28cfb623b566935c78
Priscilla Valverde <priscilla.valverde@gmail.com>
2024-02-28 18:18:33 -0600

    Adding corrections

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,10 +4,10 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.Write("What's your first name: ");
+        Console.Write("What is your first name?: ");
         string name = Console.ReadLine();
 
-        Console.Write("What's your last name: ");
+        Console.Write("What is your last name?: ");
         string last_name = Console.ReadLine();
 
         Console.WriteLine($"Your name is {last_name}, {last_name} {name}.");

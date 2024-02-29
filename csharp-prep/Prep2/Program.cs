4b77a0285a6da13d46a820d847a4f22bc3de0690
Priscilla Valverde <priscilla.valverde@gmail.com>
2024-02-28 18:56:44 -0600

    Finding Percentage

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -2,8 +2,38 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main(string [] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        
+        Console.Write("What is your grade percentage?: ");
+        string userInput = Console.ReadLine();
+        int percentage = int.Parse(userInput);
+
+        if (percentage >= 90)
+        {
+            Console.WriteLine($"Your grade {percentage} corresponds to an A.");
+        }
+        
+        else if (percentage >= 80)
+        {
+            Console.WriteLine($"Your grade {percentage} corresponds to an B.");
+        }
+        
+        else if (percentage >= 70)
+        {
+            Console.WriteLine($"Your grade {percentage} corresponds to an C.");
+        }
+        
+        else if (percentage >= 60)
+        {
+            Console.WriteLine($"Your grade {percentage} corresponds to an D.");
+        }
+
+        else
+        {
+            Console.WriteLine($"Your grade {percentage} corresponds to an F.");
+        }
+        
+
     }
 }

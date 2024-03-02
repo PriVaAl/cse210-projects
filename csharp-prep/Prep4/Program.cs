4a843db1eaf8c43a2d10034efbba7e77d440c639
Priscilla Valverde <priscilla.valverde@gmail.com>
2024-03-01 20:48:03 -0600

    Add number

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,27 @@
 using System;
+using Sytem.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        List <int> numbers = new List<int>();
+        int number = -1
+
+        while (number!= 0)
+        {
+            Console.Write( "Enter a number: ");
+            string answer = Console.ReadLine();
+            number = int.Parse(answer);
+            
+            if (number!=0)
+            {
+                numbers.Add(number);
+            }
+        }    
+
+           
+        
     }
 }

037798cfa79d54284ef5205ef142ed9d9ac11f26
Priscilla Valverde <priscilla.valverde@gmail.com>
2024-03-01 22:07:04 -0600

    Added Name and Number

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -2,9 +2,29 @@ using System;
 
 class Program
 {
+    static void Main(string[] args)
+    {
+        DisplayWelcome();
+
+        string userName = PromptUserName();
+        int userNumber = PromptUserNumber();
+
+    }
     static void DisplayWelcome()
     {
         Console.WriteLine("Welcome to the Program!");
+    }
+    static string PromptUserName()
+    {
+        Console.Write("Please enter your name: ");
+        string userName = Console.ReadLine();
+        return userName;
+    }
+    static int PromptUserNumber()
+    {
+        Console.Write("Please enter your favorite number: ");
+        int UserNumber = int.Parse(Console.ReadLine());
+        return UserNumber;
     }
     
 }

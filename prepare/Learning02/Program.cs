0217e54cce1f7553095732059cbae2fe0d70601e
Priscilla Valverde <priscilla.valverde@gmail.com>
2024-03-05 17:31:34 -0600

    Part 5

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -5,15 +5,20 @@ class Program
     static void Main(string[] args)
     {
        Job job1 = new Job();
-       job1._jobTitle = "Software Engineer"
+       job1._jobTitle = "Software Engineer";
        job1._company = "Microsoft";
        job1._enterYear = 2000;
        job1._endYear = 2024;
        
+
        Job job2 = new Job();
-       job2._jobTitle = "Product Manger";
-       job1._company = "Apple";
-       job1._enterYear = 2002;
-       job1._endYear = 2024;
+       job2._jobTitle = "Product Manager";
+       job2._company = "Apple";
+       job2._enterYear = 2002;
+       job2._endYear = 2024;
+       
+       job1.DisplayDetails();
+       job2.DisplayDetails();
+
     }
 }

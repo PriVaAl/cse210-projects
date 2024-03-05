bdd27b786a8b62c092fe755df1a4edb6095b5809
Priscilla Valverde <priscilla.valverde@gmail.com>
2024-03-05 16:19:19 -0600

    Punctuation

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,12 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
-        job1 = new Job();
-        job1._company = "Microsoft";
-        job1._jobTitle = " Software Engenieer";
-        job1._enterYear = " 2000"
-        job1._endYear = "2024"
-        job1.DisplayJobInformation();
+       Job job1 = new Job();
+       job1._jobTitle = "Software Engineer"
+       job1._company = "Microsoft";
+       job1._enterYear = 2000;
+       job1._endYear = 2024;
+       
+       Job job2 = new Job();
+       job2._jobTitle = "Product Manger";
+       job1._company = "Apple";
+       job1._enterYear = 2002;
+       job1._endYear = 2024;
     }
 }

a14096359595b4bcdf543ca59ed47a747b51d975
Priscilla Valverde <priscilla.valverde@gmail.com>
2024-03-05 15:45:25 -0600

    job information

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -5,5 +5,11 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Learning02 World!");
+        job1 = new Job();
+        job1._company = "Microsoft";
+        job1._jobTitle = " Software Engenieer";
+        job1._enterYear = " 2000"
+        job1._endYear = "2024"
+        job1.DisplayJobInformation();
     }
 }
